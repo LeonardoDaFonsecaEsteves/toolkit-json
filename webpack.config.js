ddfe65e9b67f0662d6e0e506363c8ebdc2bdaf6a
Leonardo Da Fonseca Esteves <60775829+LeonardoDaFonsecaEsteves@users.noreply.github.com>
2021-09-23 10:01:06 +0200

    build: add webpack build and engine strict

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    target: 'web',
+    mode: 'production',
+    entry: { index: './src/index.js' },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.js',
+        library: "toolkit-json",
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        umdNamedDefine: true,
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader'
+        }]
+    }
+};
