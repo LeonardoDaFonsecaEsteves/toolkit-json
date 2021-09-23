aeee82cb2b7c0f881948a8625e22aa2762b73f40
Leonardo Da Fonseca Esteves <60775829+LeonardoDaFonsecaEsteves@users.noreply.github.com>
2021-09-23 13:03:05 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,54 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: { index: './src/index.js' },
-
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'index.js',
-        library: "toolkit-json",
-        libraryTarget: 'umd',
-        globalObject: 'this',
-        umdNamedDefine: true,
+    entry: {
+        lib: './src/index.js',
     },
-
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        symlinks: false,
+    },
+    node: {
+        fs: 'empty'
+    },
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: 'babel-loader'
-        }]
-    }
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'dev')],
+                use: [
+                    {
+                        loader: 'cache-loader',
+                        options: {},
+                    },
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        library: '[name]',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, 'dist'),
+    },
+     mode: 'development',
+    devtool: 'inline-source-map',
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
+    externals: [{}]
 };
